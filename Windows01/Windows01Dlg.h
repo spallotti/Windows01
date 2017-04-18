7ce380ab796810758f97cfd9a8efe55903ecfb1f
DESKTOP-KU8VBTK\TBlackBoxLT <spallotti@gmail.com>
2017-04-18 09:08:15 +0200

    Agregar archivos de proyecto.

---
+++ Windows01/Windows01Dlg.h
@@ -0,0 +1,34 @@
+
+// Windows01Dlg.h: archivo de encabezado
+//
+
+#pragma once
+
+
+// Cuadro de diálogo de CWindows01Dlg
+class CWindows01Dlg : public CDialogEx
+{
+// Construcción
+public:
+	CWindows01Dlg(CWnd* pParent = NULL);	// Constructor estándar
+
+// Datos del cuadro de diálogo
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WINDOWS01_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// Compatibilidad con DDX/DDV
+
+
+// Implementación
+protected:
+	HICON m_hIcon;
+
+	// Funciones de asignación de mensajes generadas
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
